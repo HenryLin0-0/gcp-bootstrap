a90dd58f06e4159cefa496f1d2df5807fd4f6421
HenryLin0-0 <henry.lin@mile.cloud>
2025-10-27 23:59:31 +0800

    Create triggers.tf

---
+++ terraform/triggers.tf
@@ -0,0 +1,29 @@
+provider "google" {
+project = "gcp-prj-sys-cicd-dev"
+}
+
+
+resource "google_cloudbuild_trigger" "test_trigger" {
+name    = "test1000"
+project = "gcp-prj-sys-cicd-dev"
+location = "global"
+service_account = "projects/gcp-prj-sys-cicd-dev/serviceAccounts/297266907191-compute@developer.gserviceaccount.com"
+
+github {
+owner = "HenryLin"
+name  = "gcp-bootstrap"
+push {
+# 註釋：設定為監聽 'main' 分支的推動事件 (PUSH Event)
+branch = "main"
+# 若要監聽所有分支，請使用 branch = ".*"
+}
+}
+
+filename = "cloudbuild.yaml"
+}
+
+
+output "trigger_id" {
+description = "The unique ID of the created Cloud Build Trigger."
+value       = google_cloudbuild_trigger.test_trigger.id
+}
