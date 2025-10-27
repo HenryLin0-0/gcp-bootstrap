ff15554c78821463df91c89a17347de15c3975d9
HenryLin0-0 <henry.lin@mile.cloud>
2025-10-27 22:57:32 +0800

    Create main.tf

---
+++ terraform/main.tf
@@ -0,0 +1,24 @@
+terraform {
+required_providers {
+google = {
+source  = "hashicorp/google"
+version = "~> 5.0"
+}
+}
+}
+
+resource "google_service_account" "my_sa" {
+account_id   = "sa-cloudbuild-runner"
+display_name = "Cloud Build Runner Service Account"
+}
+
+resource "google_project_iam_member" "sa_binding" {
+project = data.google_project.current.project_id 
+role    = "roles/storage.objectViewer"
+member  = "serviceAccount:${google_service_account.my_sa.email}"
+}
+
+output "service_account_email" {
+description = "The email of the newly created service account."
+value       = google_service_account.my_sa.email
+}
